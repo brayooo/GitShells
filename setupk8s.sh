98a35027a1808a2a97f460972a738474334ce836
brayooo <bryan.lopez@uptc.edu.co>
2023-10-08 23:20:35 +0000

    fix space

--- setupk8s.sh
+++ setupk8s.sh
@@ -15,7 +15,7 @@ sudo apt install -y curl gnupg2 software-properties-common apt-transport-https c
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 
-sudo add-apt-repository "deb [arch=amd64]https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt update && sudo apt install -y containerd.io
 
